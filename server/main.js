32a60e762ca1c3d8200fa4f151b944f8240789c0
Michał Weskida <michal.weskida@gmail.com>
2017-07-29 18:53:53 +0200

    android deploy files + fixes for OLL + images for PLL (algs not done!)

--- server/main.js
+++ server/main.js
@@ -5,5 +5,6 @@ import {createAlgorithms} from './AlgorithmsCreation';
 Meteor.startup(() => {
     if (Meteor.absoluteUrl().indexOf('localhost') === -1) {
         UniConfig.private.runOnce('createAlgorithms', createAlgorithms);
+        UniConfig.private.runOnce('createAlgorithms', createAlgorithms);
     }
 });
